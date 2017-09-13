fde434727ffba8473063ce7214574f7500be159f
Layla Habahbeh <laylahabahbeh@lc-laylahabahbeh.local>
2017-09-13 12:24:07 -0700

    TDD for setting attributes

--- src/Sprite.js
+++ src/Sprite.js
@@ -2,6 +2,28 @@ export default class Sprite {
   constructor(){
     this.position = { x: 0.0, y: 0.0 }
     this.velocity = { x: 0.0, y: 0.0 }
-    this.rotation = 0
+    this.rotation = 0;
   }
+
+  get position() {
+    return this._position;
+  }
+  set position(arg) {
+    this._position = arg;
+  }
+
+  get velocity() {
+    return this._velocity;
+  }
+  set velocity(arg) {
+    this._velocity = arg;
+  }
+
+  get rotation() {
+    return this._rotation;
+  }
+  set rotation(arg) {
+    this._rotation = arg;
+  }
+
 }
--- test/SpriteTest.js
+++ test/SpriteTest.js
@@ -2,20 +2,43 @@ import { expect } from 'chai';
 import Sprite from '../src/Sprite';
 
 describe('Sprite', () => {
- describe('Instantiation', () => {
-  it('should create a new sprite with x and y coordinates that default to 0', () => {
-    const sprite = new Sprite();
-    expect(sprite.position.x).to.eql(0);
-    expect(sprite.position.y).to.eql(0);
+  describe('Setting Attributes', () => {
+    it('sets the position', () => {
+      const sprite = new Sprite();
+      sprite.position = { x: 1.0, y: 2.0 }
+      expect(sprite.position.x).to.eql(1.0);
+      expect(sprite.position.y).to.eql(2.0);
+    });
+    it('sets the velocity', () => {
+      const sprite = new Sprite();
+      sprite.velocity = { x: 1.0, y: 2.0 }
+      expect(sprite.velocity.x).to.eql(1.0);
+      expect(sprite.velocity.y).to.eql(2.0);
+    });
+    it('sets the rotation', () => {
+      const sprite = new Sprite();
+      sprite.rotation = 3.5;
+      expect(sprite.rotation).to.eql(3.5);
+    });
+
   });
-  it('should create a new sprite with x and y velocity that default to 0', () => {
-    const sprite = new Sprite();
-    expect(sprite.velocity.x).to.eql(0);
-    expect(sprite.velocity.y).to.eql(0);
+});
+
+describe('Sprite', () => {
+  describe('Instantiation', () => {
+    it('should create a new sprite with x and y coordinates that default to 0', () => {
+      const sprite = new Sprite();
+      expect(sprite.position.x).to.eql(0);
+      expect(sprite.position.y).to.eql(0);
+    });
+    it('should create a new sprite with x and y velocity that default to 0', () => {
+      const sprite = new Sprite();
+      expect(sprite.velocity.x).to.eql(0);
+      expect(sprite.velocity.y).to.eql(0);
+    });
+    it('should create a new sprite with rotation 0', () => {
+      const sprite = new Sprite();
+      expect(sprite.rotation).to.eql(0);
+    });
   });
-  it('should create a new sprite with rotation 0', () => {
-    const sprite = new Sprite();
-    expect(sprite.rotation).to.eql(0);
-  });
- });
 });
