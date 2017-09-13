32c31fb5fd0b8a1a6e4653230363b56e133bad6c
Layla Habahbeh <laylahabahbeh@lc-laylahabahbeh.local>
2017-09-13 12:45:01 -0700

    Add more validation and clean up code

--- src/Sprite.js
+++ src/Sprite.js
@@ -1,29 +1,38 @@
 export default class Sprite {
-  constructor(){
-    this.position = { x: 0.0, y: 0.0 }
-    this.velocity = { x: 0.0, y: 0.0 }
-    this.rotation = 0;
+  constructor() {
+    this._position = { x: 0.0, y: 0.0 }
+    this._velocity = { x: 0.0, y: 0.0 }
+    this._rotation = 0;
   }
-
   get position() {
     return this._position;
   }
   set position(arg) {
-    this._position = arg;
+    if(this.isValidVectorWithXY(arg) ){
+      this._position = arg;
+    }
   }
-
   get velocity() {
     return this._velocity;
   }
   set velocity(arg) {
-    this._velocity = arg;
+    if(this.isValidVectorWithXY(arg) ){
+      this._velocity = arg;
+    }
   }
-
+  isValidVectorWithXY(arg){
+    return typeof arg == "object"
+      && arg.x
+      && arg.y
+      && typeof arg.x == "number"
+      && typeof arg.y == "number";
+  }
   get rotation() {
     return this._rotation;
   }
   set rotation(arg) {
-    this._rotation = arg;
+    if (typeof(arg) == "number") {
+      this._rotation = arg;
+    }
   }
-
 }
--- test/SpriteTest.js
+++ test/SpriteTest.js
@@ -2,29 +2,61 @@ import { expect } from 'chai';
 import Sprite from '../src/Sprite';
 
 describe('Sprite', () => {
-  describe('Setting Attributes', () => {
-    it('sets the position', () => {
+  describe('setting attributes', () => {
+    it('sets the position when the argument is valid', () => {
       const sprite = new Sprite();
       sprite.position = { x: 1.0, y: 2.0 }
       expect(sprite.position.x).to.eql(1.0);
       expect(sprite.position.y).to.eql(2.0);
     });
-    it('sets the velocity', () => {
+    it('doesn\'t set the position when the argument is a string', () => {
+      const sprite = new Sprite();
+      sprite.position = "hello";
+      expect(sprite.position.x).to.eql(0.0);
+      expect(sprite.position.y).to.eql(0.0);
+    });
+    it('doesn\'t set the position when the argument doesn\'t have the right values', () => {
+      const sprite = new Sprite();
+      sprite.position = { q: 1, z: 1};
+      expect(sprite.position.x).to.eql(0.0);
+      expect(sprite.position.y).to.eql(0.0);
+    });
+    it('sets the velocity when the argument is valid', () => {
       const sprite = new Sprite();
       sprite.velocity = { x: 1.0, y: 2.0 }
       expect(sprite.velocity.x).to.eql(1.0);
       expect(sprite.velocity.y).to.eql(2.0);
     });
-    it('sets the rotation', () => {
+    it('doesn\'t set the velocity when the argument is a string', () => {
+      const sprite = new Sprite();
+      sprite.velocity = "hello";
+      expect(sprite.velocity.x).to.eql(0.0);
+      expect(sprite.velocity.y).to.eql(0.0);
+    });
+    it('doesn\'t set the velocity when the argument doesn\'t have the right values', () => {
+      const sprite = new Sprite();
+      sprite.velocity = { q: 1, z: 1};
+      expect(sprite.velocity.x).to.eql(0.0);
+      expect(sprite.velocity.y).to.eql(0.0);
+    });
+    it('doesn\'t set the velocity when the arguments aren\'t number', () => {
+      const sprite = new Sprite();
+      sprite.velocity = { x: 1, y: "hello"};
+      expect(sprite.velocity.x).to.eql(0.0);
+      expect(sprite.velocity.y).to.eql(0.0);
+    });
+    it('sets the rotation when the argument is valid', () => {
       const sprite = new Sprite();
       sprite.rotation = 3.5;
       expect(sprite.rotation).to.eql(3.5);
     });
-
+    it('doesn\'t set the rotation when the argument is in valid', () => {
+      const sprite = new Sprite();
+      sprite.rotation = "hello";
+      expect(sprite.rotation).to.eql(0.0);
+    });
   });
-});
 
-describe('Sprite', () => {
   describe('Instantiation', () => {
     it('should create a new sprite with x and y coordinates that default to 0', () => {
       const sprite = new Sprite();
